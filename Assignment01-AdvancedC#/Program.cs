bead791ce673d71befe8ef927cd7f5baf2e58f53
Marwa Mahmoud <mm3764453@gmail.com>
2024-08-26 12:34:28 +0300

    Question2

--- Assignment01-AdvancedC#/Program.cs
+++ Assignment01-AdvancedC#/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Assignment01_AdvancedC_.Question1;
+using Assignment01_AdvancedC_.Question2;
 
 namespace Assignment01_AdvancedC_
 {
@@ -14,6 +15,19 @@ namespace Assignment01_AdvancedC_
             BubbleSort.OptimizedBubbleSort(intArray);
             Console.WriteLine("\nSorted integer array:");
             PrintArray(intArray);
+            #endregion
+            #region Q2
+            // Example with integer range
+            Range<int> intRange = new Range<int>(11, 5);
+            Console.WriteLine($"Is 10 in range? {intRange.IsInRange(10)}"); 
+            Console.WriteLine($"Is 20 in range? {intRange.IsInRange(20)}"); 
+            Console.WriteLine($"Length of integer range: {intRange.Length()}"); 
+
+            // Example with double range
+            Range<double> doubleRange = new Range<double>(8.9, 2.5);
+            Console.WriteLine($"Is 5.0 in range? {doubleRange.IsInRange(5.0)}"); 
+            Console.WriteLine($"Is 9.0 in range? {doubleRange.IsInRange(9.0)}"); 
+            Console.WriteLine($"Length of double range: {doubleRange.Length()}"); 
             #endregion
         }
         #region Q1
---
+++ Assignment01-AdvancedC#/Question2/Range.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01_AdvancedC_.Question2
+{
+    internal class Range<T> where T : IComparable<T>
+    {
+        public T Min { get; set; }
+        public T Max { get; set; }
+        public Range(T max, T min)
+        {
+            if (min.CompareTo(max) > 0)
+            {
+                Console.WriteLine("Minimum value should not be greater than maximum value.");
+            }
+            Max = max;
+            Min = min;
+        }
+        public bool IsInRange(T value)
+        {
+            if (value is not null)
+            {
+                return value.CompareTo(Min) >= 0 && value.CompareTo(Max) <= 0;
+            }
+            return false;
+        }
+        public T Length()
+        {
+            return (dynamic)Max - (dynamic)Min;
+        }
+    }
+}
